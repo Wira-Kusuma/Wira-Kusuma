fe2f7e37ea63960bf003692528ec30ce1083ded6
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-06-20 12:56:58 +0700

    scroll animation

--- script.js
+++ script.js
@@ -15,3 +15,15 @@ window.addEventListener("load", function () {
         img.style.transform = "rotate(-15deg) scale(1.2)";
     }, 1000); // dalam 1 detik
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        }
+      });
+    }, { threshold: 0.1 });
+
+    document.querySelectorAll('.fade-in').forEach(el => observer.observe(el));
+});
