1867a4743ba0ac9913a180d825c61ff49d9ef4ef
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-22 21:14:33 +0700

    add main and toggle nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import { useState } from 'react'
 import './index.css'
+import { useEffect } from 'react'
 
 export default function App() {
 
   return (
     <>
     <Header />
+    <Main />
     </>
   )
 }
 
 function Header() {
+
+  const [showed, setShowed] = useState(false)
+
   return(
     <header>
       <h1>Wira</h1>
-      <nav>
+      <div className='burger-menu' onClick={()=>setShowed(!showed)} >
+        &#8801;
+      </div>
+      <nav className={showed ? 'active' : ''} >
         <ul>
           <li><a href="#about">About</a></li>
           <li><a href="#skill">skill</a></li>
@@ -21,5 +30,22 @@ function Header() {
         </ul>
       </nav>
     </header>
+  )
+}
+
+function Main() {
+  return(
+    <main>
+      <div className='main1'>
+        <p>Hi!, Im</p>
+        <h1>
+          Wira Kusuma Phandawa
+        </h1>
+      </div>
+
+      <div className='main2'>
+
+      </div>
+    </main>
   )
 }
