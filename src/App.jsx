1fa00a417d5681a6ff65598e4721a9eb92f551d3
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-22 12:22:02 +0700

    provide header

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,22 @@ export default function App() {
 
   return (
     <>
+    <Header />
     </>
   )
 }
 
+function Header() {
+  return(
+    <header>
+      <h1>Wira</h1>
+      <nav>
+        <ul>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skill">skill</a></li>
+          <li><a href="#project">project</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
