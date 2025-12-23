653286ee707e556b4a548c7b1eec57f2e24811a4
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-23 09:23:12 +0700

    aboutme section

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ export default function App() {
     <>
     <Header />
     <Main />
+    <About />
     </>
   )
 }
@@ -37,15 +38,35 @@ function Main() {
   return(
     <main>
       <div className='main1'>
-        <p>Hi!, Im</p>
-        <h1>
-          Wira Kusuma Phandawa
-        </h1>
+        <div>
+          <p>Hi!, Im</p>
+          <h1>
+            Wira Kusuma Phandawa
+          </h1>
+          <button>Let's talk</button>
+          <button>Show project</button>
+        </div>
       </div>
 
       <div className='main2'>
 
       </div>
     </main>
+  )
+}
+
+function About(){
+  return(
+    <section className='about' id='about'>
+      <div className="about1">
+        <h1>About Me</h1>
+        <p>I am a Frontend Developer with a strong passion for building clean, responsive, and user-friendly web interfaces. I work with HTML, CSS, and JavaScript to transform designs into well-structured and maintainable code.</p>
+        <p>I enjoy learning new technologies, improving my problem-solving skills, and paying close attention to UI details to deliver the best user experience. For me, coding is not just about making things work, but about creating efficient and scalable solutions.</p>
+        <p>I enjoy learning new technologies, improving my problem-solving skills, and paying close attention to UI details to deliver the best user experience. For me, coding is not just about making things work, but about creating efficient and scalable solutions.</p>
+      </div>
+      <div className="about2">
+        <img src="img/wira-removebg-preview.png" alt="" />
+      </div>
+    </section>
   )
 }
