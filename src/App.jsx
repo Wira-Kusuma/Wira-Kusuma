417fd9a0aad8b194b5112f625433129b1b0022bd
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2025-12-24 12:24:43 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function Header() {
 
   return(
     <header>
-      <h1>Wira</h1>
+      <h1>wiraKusuma</h1>
       <div className='burger-menu' onClick={()=>setShowed(!showed)} >
         &#8801;
       </div>
@@ -42,13 +42,14 @@ function Main() {
           <h1>
             Wira Kusuma Phandawa
           </h1>
+          <p>Based in the Indonesia, I'm a front-end developer passionate about building accessible web apps that users love.</p>
           <button>Let's talk</button>
           <button>Show project</button>
         </div>
       </div>
 
       <div className='main2'>
-
+        <img src="img/wira.jpg" alt="portait of wira" />
       </div>
     </main>
   )
@@ -58,13 +59,13 @@ function About(){
   return(
     <section className='about' id='about'>
       <div className="about1">
-        <h1>About Me</h1>
-        <p>I am a Frontend Developer with a strong passion for building clean, responsive, and user-friendly web interfaces. I work with HTML, CSS, and JavaScript to transform designs into well-structured and maintainable code.</p>
-        <p>I enjoy learning new technologies, improving my problem-solving skills, and paying close attention to UI details to deliver the best user experience. For me, coding is not just about making things work, but about creating efficient and scalable solutions.</p>
-        <p>I enjoy learning new technologies, improving my problem-solving skills, and paying close attention to UI details to deliver the best user experience. For me, coding is not just about making things work, but about creating efficient and scalable solutions.</p>
+        <div>
+          <h1>About Me</h1>
+          <p>Hi!, im Wira Kusuma Phandawa, a front-end developer based in indonesia. im focus experiencing in web dev, problem solving, clean code. my main focus is build fast web app, safe, and scalable for you.</p>
+        </div>
       </div>
       <div className="about2">
-        <img src="img/wira-removebg-preview.png" alt="portait of wira" />
+        <img src="img/react.png" alt="react icon" />
       </div>
     </section>
   )
