4469effc3069cd936e9e6ce9ced43e1a3af5a474
Wira Kusuma Phandawa <wirakusuma5566@gmail.com>
2026-01-02 13:15:06 +0700

    update header

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import "../styles/globals.css";
 
 export const metadata = {
   title: "Wira kusuma's Portofolio",
@@ -19,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         {children}
       </body>
     </html>
---
+++ src/components/components.jsx
@@ -0,0 +1,34 @@
+"use client"
+import { useState } from "react";
+import "../styles/globals.css";
+import header from "../styles/header.module.css";
+
+export function Header() {
+    const [open, setOpen] = useState(false);
+  return (
+    <header className={header.header}>
+      <h1>Wira</h1>
+
+      <div 
+      className={header.burgerMenu} 
+      onClick={()=>setOpen(!open)}
+      >
+        &#x2630;
+      </div>
+
+      <nav className={`${header.nav} ${open ? header.show : ""}`}>
+        <ul>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Skills</a>
+          </li>
+          <li>
+            <a href="#">Project</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
